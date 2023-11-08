d4c7a0b31527baf1ea7e2be50421791829ba73e9
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-11-08 11:45:23 +0100

    added navigation >_<

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginPage from "./src/components/pages/LoginPage";
 import HomePage from "./src/components/pages/HomePage";
 import { PaperProvider } from "react-native-paper";
 import PokedexPage from "./src/components/pages/PokedexPage";
+import ProfilePage from "./src/components/pages/ProfilePage";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreateEditPage from "./src/components/pages/CreateEditPage";
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -13,18 +15,24 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <PaperProvider>
-       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomePage} />
-          <Tab.Screen name="Login" component={LoginPage} />
-          <Tab.Screen name="Pokédex" component={PokedexPage} />
-        </Tab.Navigator>
-        </NavigationContainer>
-        <NavigationContainer>
-        <Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Main" component={MainTabs} />
+          <Stack.Screen name="CreateEdit" component={CreateEditPage} />
           <Stack.Screen name="Login" component={LoginPage} />
         </Stack.Navigator>
-        </NavigationContainer>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
+
+function MainTabs() {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomePage} options={{ tabBarIcon: 'home' }} />
+      <Tab.Screen name="Pokédex" component={PokedexPage} options={{ tabBarIcon: 'book' }} />
+      <Tab.Screen name="Profile" component={ProfilePage} options={{ tabBarIcon: 'account' }} />
+    </Tab.Navigator>
+  );
+}
+
