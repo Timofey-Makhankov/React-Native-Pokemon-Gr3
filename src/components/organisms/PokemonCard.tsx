acb1846a38d0c513c2e39bc3817c4b4e3557b896
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-11-09 13:07:58 +0100

    delete dialog und homepage

--- src/components/organisms/PokemonCard.tsx
+++ src/components/organisms/PokemonCard.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { Card, Text, IconButton } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { Card, Text, IconButton, Dialog, Paragraph, Portal, Button } from "react-native-paper";
 import PokemonType from "../../Types/PokemonType";
 import { FlatList, SafeAreaView, View, StyleSheet } from "react-native";
 import ElementType from "../../Types/ElementType";
@@ -9,70 +9,112 @@ import { getContainerColorFromType } from "../../util/ColorFromType";
 import CardDetail from "../molecules/CardDetail";
 import { useNavigation } from "@react-navigation/native";
 
-export default function PokemonCard({ pokemonData }: { pokemonData: PokemonType }) {
-    const navigation = useNavigation();
-    const styles = StyleSheet.create({
-        card: {
-            borderWidth: 4
-        },
-        cardImage: {
-            //borderColor: "white",
-            //objectFit: 'scale-down',
-            //paddingHorizontal: 4
-        },
-        typeContainer: {
-            marginTop: 8,
-            width: '100%',
-            flexDirection: 'row'
-        },
-        icons: {
-            borderWidth: 0,
-        },
-    });
+export default function PokemonCard({
+  pokemonData,
+}: {
+  pokemonData: PokemonType;
+}) {
+  const navigation = useNavigation();
+  const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
+  const styles = StyleSheet.create({
+    card: {
+      borderWidth: 4,
+    },
+    cardImage: {
+      //borderColor: "white",
+      //objectFit: 'scale-down',
+      //paddingHorizontal: 4
+    },
+    typeContainer: {
+      marginTop: 8,
+      width: "100%",
+      flexDirection: "row",
+    },
+    icons: {
+      borderWidth: 0,
+    },
+  });
 
-    const navigateToCreateEdit = () => {
-        navigation.navigate('CreateEdit' as never);
-      };
+  const navigateToCreateEdit = () => {
+    navigation.navigate("CreateEdit" as never);
+  };
+  const toggleDeleteDialog = () => {
+    setDeleteDialogVisible(!deleteDialogVisible);
+  };
 
-    const bgColor: AlphaColor = getContainerColorFromType(pokemonData.type[0])
-    const _renderItem = ({ item }: { item: ElementType }) => (
-        <TypeChip type={item} />
-    );
+  const handleDelete = () => {
+    console.log("deleted pokemon");
+  }
 
-    return (
-        <View style={{ backgroundColor: '#fff', borderRadius: 15 }}>
-        <Card mode="outlined" style={[styles.card, {
+  const bgColor: AlphaColor = getContainerColorFromType(pokemonData.type[0]);
+  const _renderItem = ({ item }: { item: ElementType }) => (
+    <TypeChip type={item} />
+  );
+
+  return (
+    <View style={{ backgroundColor: "#fff", borderRadius: 15 }}>
+      <Card
+        mode="outlined"
+        style={[
+          styles.card,
+          {
             borderColor: `rgba(${bgColor.red}, ${bgColor.green}, ${bgColor.blue}, 0.8)`,
             backgroundColor: `rgba(${bgColor.red}, ${bgColor.green}, ${bgColor.blue}, 0.40)`,
-        }]}>
-            <Card.Title
-                titleVariant="headlineMedium"
-                subtitleVariant="labelLarge"
-                title={pokemonData.name.english}
-                subtitle={`Pokédex: #${pokemonData.id}`}
-            />
-            <Card.Cover resizeMode="contain"
-                source={{
-                    uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(
-                        "000" + pokemonData.id
-                    ).slice(-3)}.png`,
-                }}
-            />
-            <Card.Content>
-                <FlatList 
-                horizontal 
-                data={pokemonData.type} 
-                renderItem={_renderItem} 
-                ItemSeparatorComponent={() => (<View style={{ width: 16 }} />)}
-                style={{ paddingVertical: 16 }} 
-                />
-                <CardDetail info={pokemonData.base} />
-            </Card.Content>
-            <Card.Actions>
-                <IconButton icon="delete" mode="outlined" onPress={(event) => {}} style={styles.icons}/>
-                <IconButton icon="pencil" mode="outlined" onPress={navigateToCreateEdit} style={styles.icons}/>
-            </Card.Actions>
-        </Card>
-        </View>
-    );
+          },
+        ]}
+      >
+        <Card.Title
+          titleVariant="headlineMedium"
+          subtitleVariant="labelLarge"
+          title={pokemonData.name.english}
+          subtitle={`Pokédex: #${pokemonData.id}`}
+        />
+        <Card.Cover
+          resizeMode="contain"
+          source={{
+            uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(
+              "000" + pokemonData.id
+            ).slice(-3)}.png`,
+          }}
+        />
+        <Card.Content>
+          <FlatList
+            horizontal
+            data={pokemonData.type}
+            renderItem={_renderItem}
+            ItemSeparatorComponent={() => <View style={{ width: 16 }} />}
+            style={{ paddingVertical: 16 }}
+          />
+          <CardDetail info={pokemonData.base} />
+        </Card.Content>
+        <Card.Actions>
+          <IconButton
+            icon="delete"
+            mode="outlined"
+            onPress={toggleDeleteDialog}
+            style={styles.icons}
+          />
+          <IconButton
+            icon="pencil"
+            mode="outlined"
+            onPress={navigateToCreateEdit}
+            style={styles.icons}
+          />
+        </Card.Actions>
+      </Card>
+
+      <Portal>
+        <Dialog visible={deleteDialogVisible} onDismiss={toggleDeleteDialog}>
+          <Dialog.Title>Confirm Deletion</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>Are you sure you want to delete this Pokémon?</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={toggleDeleteDialog}>Cancel</Button>
+            <Button onPress={handleDelete} labelStyle={{ color: 'red' }}>Delete</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
+  );
 }
